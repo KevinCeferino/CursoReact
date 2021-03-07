1c156345d01e89544a8874e59c95fd0af917c3ee
KevinCeferino <kevin.ceferino000@gmail.com>
2021-03-07 01:55:46 -0500

    segundo commir

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css';
-import Hola from './components/hola'
 
 function App() {
   return (
     <div className="App">
       <h1>Mi primer Proyecto</h1>
-      <Hola></Hola>
     </div>
   );
 }
